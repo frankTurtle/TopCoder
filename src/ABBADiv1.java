c7c63a4ce71bcebb74807b9777265e71f33a93f1
Barret J. Nobel <bear.nobel@gmail.com>
2016-02-17 14:40:41 -0500

    not quite working, but closer

--- src/ABBADiv1.java
+++ src/ABBADiv1.java
@@ -2,21 +2,33 @@
  * Created by BigB on 2/17/16.
  */
 public class ABBADiv1{
-    public String canObtain( String initial, String target ){
-        String initialCopy = initial;
 
-        if( initialCopy.length() == target.length() ){
-            return ( initialCopy.equals(target) ) ? "Possible" : "Impossible";
-        }
-        else{
-            if( isSubString(reverse(initialCopy), target) ) return canObtain( reverse(initialCopy), target );
-            if( isSubString(initialCopy += "A", target) ) return canObtain( initialCopy += "A", target );
-        }
+    public static void main( String[] args ){
 
-        return "here";
+        System.out.println( canObtain("AAABBAABB", "BAABAAABAABAABBBAAAAAABBAABBBBBBBABB") );
+
     }
 
-    private String reverse( String addToMe ){
+    public static String canObtain( String initial, String target ){
+        if( initial.length() == target.length() ){
+            return ( initial.equals(target) ) ? "Possible" : "Impossible";
+        }
+        else{
+                String addA = initial + "A";
+                String addB = reverse( initial );
+
+                if( isSubString(addB, target) )
+                    return canObtain( addB, target );
+
+                else if( isSubString(addA, target) ){
+                    return canObtain( addA, target );
+                }
+        }
+
+        return "Impossiblee";
+    }
+
+    private static String reverse( String addToMe ){
         String returnString = "B";
 
         for( int i = addToMe.length(); i > 0; i-- ){
@@ -26,12 +38,21 @@ public class ABBADiv1{
         return returnString;
     }
 
-    private boolean isSubString( String check, String against ){
-        for( int i = 0; i <= (against.length() - check.length()); i++ ){
-            if( against.substring(i, (check.length()) ).equals(check) )
-                return true;
-        }
-        return false;
+    private static boolean isSubString( String check, String against ){
+
+        return against.toLowerCase().contains(check.toLowerCase());
+//
+//        System.out.println( "check: " + check + " " + check.length());
+//        System.out.println( "against: " + against + " " + against.length());
+//        for( int i = 0; i <= (against.length() - check.length()); i++ ){
+//
+//            System.out.println( i );
+//
+//            if( against.substring(i, check.length()).equals(check) )
+//                return true;
+//        }
+//
+//        return false;
     }
 
 }
